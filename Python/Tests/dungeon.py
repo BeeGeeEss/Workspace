a38486acd680b10ec86f9dd80e541e8e066551c4
BeeGeeEss <brando.g.smith@gmail.com>
2025-04-23 21:05:17 +1000

    Python excercises

---
+++ Python/Tests/dungeon.py
@@ -0,0 +1,21 @@
+def defeat_monsters_in_dungeon():
+    dungeon = [
+        "Goblin", 
+        "Gold coins", 
+        "Dragon",
+        "Dragon",
+        "Bandit",
+        "Gold coins",
+        "Giant Snake"]
+    # Defeat the monsters!
+
+
+
+    dungeon[0] = "Gold coins"
+    dungeon[2] = "Gold coins"
+    dungeon[3] = "Gold coins"
+    dungeon[4] = "Gold coins"
+    dungeon[6] = "gold coins"
+    print(dungeon)
+
+defeat_monsters_in_dungeon()
---
+++ Python/Tests/tests.py
@@ -0,0 +1,30 @@
+first_name = "Eric"
+famous_person = " Kate Miller-Heidke "
+famous_person = famous_person.strip()
+message = "I don't need to sleep, to dream"
+filename = 'tests.txt'
+website = 'https://google.com'
+
+print(f"Hello {first_name}, would you like to learn some Python?")
+
+print(first_name.title())
+print(first_name.upper())
+print(first_name.lower())
+print("Kate Miller-Heidke once said 'I don't need to sleep, to dream'")
+print(f"{famous_person} once said '{message}'")
+print(famous_person)
+print(famous_person.rstrip())
+print(famous_person.lstrip())
+print(famous_person.strip())
+print("Kate\nMiller\tHeidke")
+print(website.removeprefix('https://'))
+print(filename.removesuffix('.txt'))
+
+
+favourite_number = 63
+
+print(4+4)
+print(10-2)
+print(4*2)
+print(24/3)
+print(f"My favpurite number is... drumroll please.... {favourite_number}!")
