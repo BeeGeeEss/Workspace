97ab345ccb5f00267ca95014e7a551c1a98a3463
BeeGeeEss <brando.g.smith@gmail.com>
2025-05-08 11:23:12 +1000

    Python sums

--- Python/Tests/sums.py
+++ Python/Tests/sums.py
@@ -1,10 +1,12 @@
-#This is stock standard addition:
+# #This is stock standard addition:
 
 # def add_numbers(x, y):
 #     return x + y
 
 
 
+#Use a while loop to write a function that calculates and returns the sum of 
+# all integers up to and including 100.
 
 # def sum_to_one_hundred():
 
@@ -22,7 +24,8 @@
 # print(sum_to_one_hundred())
 
 
-
+#Specifically, can you make it so that the function calculates the sum 
+# of all integers up to (and including) a target value
 
 # def sum_to_x(target_integer):
     
@@ -39,6 +42,10 @@
 # print(sum_to_x(4))
 # print(sum_to_x(100))
 
+
+#Write a function that asks the user to input a number from 1 to 100 
+# (inclusive). If the user supplies a value outside that range, the function 
+# should print an error message and prompt the user to try again
 
 # def input_within_range():
 #     MIN_VALUE = 1
@@ -59,24 +66,87 @@
 
 
 
-def calculate_total_cost_of_visiting_cinema(age, has_membership, transport_type):
-    entry_ticket_cost = 10 if age >= 60 else 15
-    candy_cost = 5 if has_membership else 10
-    parking_cost = 3 if transport_type == "Car" else 0
+# def calculate_total_cost_of_visiting_cinema(age, has_membership, transport_type):
+#     entry_ticket_cost = 10 if age >= 60 else 15
+#     candy_cost = 5 if has_membership else 10
+#     parking_cost = 3 if transport_type == "Car" else 0
 
-    result = entry_ticket_cost, candy_cost, parking_cost 
+#     result = entry_ticket_cost, candy_cost, parking_cost 
+#     print(result)
+
+#     result = entry_ticket_cost+ candy_cost+ parking_cost 
+
+#     print(result)
+
+# calculate_total_cost_of_visiting_cinema(20, True, "Car")
+# calculate_total_cost_of_visiting_cinema(65, False, "Bus")
+# calculate_total_cost_of_visiting_cinema(45, False, "Car")
+# calculate_total_cost_of_visiting_cinema(60, True, "Car")
+
+
+
+
+#Use a for loop to write a function that calculates and returns the sum of 
+# all integers up to and including 100.
+
+# def sum_to_one_hundred():
+#     result = 0
+#     # insert your code here...
+#     for x in range(1, 101, 1):
+#         result += x
+#     return result
+
+# result = sum_to_one_hundred()
+# print(result)
+
+
+
+#Write a function that repeatedly asks the user for an integer, 
+# adding that number to a total each time, until the user gives the input "end"
+
+# def sum_of_inputs():
     
-    print(result)
+#     total = 0
+#     user_number = 0
 
-    result = entry_ticket_cost+ candy_cost+ parking_cost 
+#     # your code here...
+#     while True:
+#         user_number = input("Enter a number (or 'end' to finish): ")
+#         if user_number == 'end':
+#             break
+#         total += int(user_number)
+#     return total
 
-    print(result)
+# print(sum_of_inputs())
 
-calculate_total_cost_of_visiting_cinema(20, True, "Car")
-calculate_total_cost_of_visiting_cinema(65, False, "Bus")
-calculate_total_cost_of_visiting_cinema(45, False, "Car")
-calculate_total_cost_of_visiting_cinema(60, True, "Car")
 
+#Create a function that sums every integer up to the value it receives 
+#as an argument.
+
+# def sum_to_x(target_integer):
+#     result = 0
+
+#     for x in range(1, target_integer +1):
+#         result += x
+#     return result
+
+# result = sum_to_x(4)
+# print(result)
+
+
+
+def sum_of_evens(input_list):
+    result = 0
+    
+    # your code here...
+    for element in input_list:
+        if element % 2 == 0:
+            result += element
+    return result
+
+print(sum_of_evens([]))
+print(sum_of_evens([7, 9, 11]))
+print(sum_of_evens([2, 6, 14]))
 
 
 
