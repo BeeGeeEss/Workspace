56742958d782944efa7eeb42a44ff878fd1d07ed
BeeGeeEss <brando.g.smith@gmail.com>
2025-05-13 17:23:41 +1000

    Change folder

---
+++ Python/Tests/pseudocode.py
@@ -0,0 +1,31 @@
+# def current_day(user_input):
+
+#     if user_input == "Wednesday":
+#         print("It's Wednesday!")
+    
+#     else:
+#         user_input = " "
+#         while not user_input.lower().endswith("y"):
+#             user_input = input("What day is it?: ")
+        
+#         if user_input == "Wednesday":
+#             print("Haha, you're a funny one! See ya!")
+#         else:
+#             print(f"Thankyou for letting me know that today is {user_input}! See ya!")
+
+
+# current_day("Wednesday")
+# current_day(" ")
+# current_day("Thursday")
+
+
+
+def square_the_numbers(numbers_list):
+    
+    squares = []
+
+    for number in numbers_list:
+        squares.append(number ** 2)
+        return squares
+
+square_the_numbers([1,2,3,4,5])
